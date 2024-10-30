b1efbcad19eb4a66c220df2e8dc693d9101d699a
Blake Ziegler <zieglerblake2@gmail.com>
2024-10-30 19:04:53 -0400

    updated NN docs

--- src/lineGenerationNN.py
+++ src/lineGenerationNN.py
@@ -1,27 +1,43 @@
 # Line Generation Neural Network
 
+
+import pandas as pd
+import numpy as np
+
 '''
-FEATURE SCALING & ENCODING
+        FEATURE SCALING & ENCODING
+- one-hot encode categorical variables
+- normalize numericals between 0 and 1 (relu) or -1 and 1 (sigmoid)
+
+
 '''
 
 
 '''
-NEURAL NETWORK SETUP
+        NEURAL NETWORK SETUP
+- Split (80/20) and shuffle data
+- Define layer dimensions
+- initialize parameters
 '''
 
 '''
-ACTIVATION FUNCTIONS
+        ACTIVATION FUNCTIONS
+- Define linear and relu algorithms along w/ their derivatives
 '''
 
 '''
-FORWARD PROP
+        FORWARD PROP
+- Define input -> output algorithm
+- compute cost function
 '''
 
 '''
-BACK PROP
+        BACK PROP
+- Move back through network driven by cost value
+- Update parameters
 '''
 
 
 '''
-TRAINING AND OPTIMIZATION
+        TRAINING AND OPTIMIZATION
 '''
