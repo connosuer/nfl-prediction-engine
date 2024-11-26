949cfa7de899dcac7c34d247b0b14548f986b58f
Blake Ziegler <zieglerblake2@gmail.com>
2024-11-26 18:49:22 -0500

    finished forward prop

--- src/lineGenerationNN.py
+++ src/lineGenerationNN.py
@@ -43,31 +43,75 @@ class NeuralNetwork:
             params['b' + str(layer)] = np.zeros((self.layers[layer], 1))
             return params
 
+    def _init_velo(self):
+        velo = {}
+        L = len(self.layers)
+        for layer in range(1, L):
+            velo['dW'+ str(layer)] = np.zeros_like(self.params['W' + str(layer)])
+            velo['db'+ str(layer)] = np.zeros_like(self.params['b' + str(layer)])
+            print(velo)
+        return velo
 
-'''
-        FORWARD PROP
-- Define input -> output algorithm
-- compute cost function
-'''
 
-
-
-
-
-'''
+    '''
         ACTIVATION FUNCTIONS
-- Define linear and relu algorithms along w/ their derivatives
-'''
+    - Define linear and relu algorithms along w/ their derivatives
+    '''
 
+    def _relu(self, ):
 
+    def _relu_derivative(self, ):
 
-'''
+    def _linear(self, ):
+
+    '''
+        FORWARD PROP
+    - Define input -> output algorithm
+    - compute cost function
+    '''
+    def forward_feed(self, X):
+        # Set network params
+        A = X.T
+        self.cache['A'] = A
+        L = len(self.layers) - 1
+        func, _ = self.activation
+
+        # Hidden Layers
+        for layer in range(1, L):
+            W = self.params['W' + str(layer)]
+            b = self.params['b' + str(layer)]
+            Z = np.dot(W, A) + b
+            A = func(Z)
+            self.cache['A' + str(layer)] = A
+            self.cache['Z' + str(layer)] = Z
+
+        # Output Layer
+        W = self.params['W' + str(L)]
+        b = self.params['b' + str(L)]
+        Z = np.dot(W, A) + b
+        A = self._linear
+        self.cache['A' + str(L)] = A
+        self.cache['Z' + str(L)] = Z
+
+    def cost(self, y1, y2):
+        slope = y2.shape[1]
+        C = np.sum((y1 - y2) ** 2 / (2 * slope))
+        return C
+
+    '''
         BACK PROP
-- Move back through network driven by cost value
-- Update parameters
-'''
+    - Move back through network driven by cost value
+    - Update parameters
+    '''
+
+    def backward_feed(self, ):
+
+    def update_network(self):
 
 
-'''
+
+    '''
         TRAINING AND OPTIMIZATION
-'''
+    '''
+
+    def train(self):
